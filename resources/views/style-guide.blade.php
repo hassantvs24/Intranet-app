7c16fb656a11cb25ab97c3d29f6777283a00ed2c
walidmahade <walid_mahade@outlook.com>
2020-08-08 15:35:30 +0600

    style and auth template setup

---
+++ resources/views/style-guide.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1 class="display-1">Hello World!!</h1>
+    <h1 class="display-2">Hello World!!</h1>
+    <h1 class="display-3">Hello World!!</h1>
+    <h1 class="display-4">Hello World!!</h1>
+    <hr>
+    <div class="cta">
+        <a href="#" class="btn btn-primary">Click Here</a>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// default page -> dashboard / login
+Route::get('/', 'HomeController@index');
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/sg', function () {
+    return view('style-guide');
 });
