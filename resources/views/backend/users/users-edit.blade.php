c910279bd54590827f0ad8405c20869fcf75949a
walidmahade <walid_mahade@outlook.com>
2020-08-13 15:31:28 +0600

    backend crud pages

--- resources/views/backend/users/users-edit.blade.php
+++ resources/views/backend/users/users-edit.blade.php
@@ -1,15 +1,13 @@
 @extends('layouts.app-admin')
-
 @section('body-class', 'bg-light')
 
 @section('admin-content')
-    <div class="group-create-wrap">
-
+    <div class="users-create-wrap">
         {{-- START create group --}}
         <div class="container" id="create-group-form-wrap">
             <div class="row page-header my-4 pt-4">
                 <div class="col">
-                    <h3 class="page-title">{{ __('Edit user')}}</h3>
+                    <h3 class="page-title">{{ __('Edit user') }}</h3>
                     <hr>
                 </div>
             </div>
@@ -19,108 +17,88 @@
                     <form action="#" method="post">
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="group_name">Name</label>
-                                <input type="text" class="form-control" id="group_name" name="group_name" value="dynamic_group_name" required>
+                                <label for="group_name">{{ __('Name') }}</label>
+                                <input type="text" class="form-control" id="group_name" name="group_name" value="Adam Levi" required>
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="form-group col-md-6 d-flex flex-wrap">
-                                <label for="" class="d-block w-100">Select a group color:</label>
+                                <label for="" class="d-block w-100">{{ __('Assign a group:') }}</label>
 
-                                <div class="custom-control custom-radio mr-4 my-1 body-green">
-                                    <input type="radio" id="group_color_green" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_green">Green</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
+                                <div class="custom-control custom-radio mr-4 my-1">
+                                    <input type="radio" id="group_a" name="user_group" class="custom-control-input" checked>
+                                    <label class="custom-control-label" for="group_a">Group A</label>
                                 </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-blue">
-                                    <input type="radio" id="group_color_blue" name="group_color" class="custom-control-input" checked>
-                                    <label class="custom-control-label" for="group_color_blue">Blue</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
+                                <div class="custom-control custom-radio mr-4 my-1">
+                                    <input type="radio" id="group_b" name="user_group" class="custom-control-input">
+                                    <label class="custom-control-label" for="group_b">Group B</label>
                                 </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-yellow">
-                                    <input type="radio" id="group_color_yellow" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_yellow">Yellow</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
+                                <div class="custom-control custom-radio mr-4 my-1">
+                                    <input type="radio" id="group_c" name="user_group" class="custom-control-input">
+                                    <label class="custom-control-label" for="group_c">Group C</label>
                                 </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-red">
-                                    <input type="radio" id="group_color_red" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_red">Red</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
+                                <div class="custom-control custom-radio mr-4 my-1">
+                                    <input type="radio" id="group_d" name="user_group" class="custom-control-input">
+                                    <label class="custom-control-label" for="group_d">Group D</label>
                                 </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-pink">
-                                    <input type="radio" id="group_color_pink" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_pink">Pink</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
-                                </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-orange">
-                                    <input type="radio" id="group_color_orange" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_orange">Orange</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
-                                </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-teal">
-                                    <input type="radio" id="group_color_teal" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_teal">Teal</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
-                                </div>
-                                <div class="custom-control custom-radio mr-4 my-1 body-violet">
-                                    <input type="radio" id="group_color_violet" name="group_color" class="custom-control-input">
-                                    <label class="custom-control-label" for="group_color_violet">Violet</label>
-                                    <span class="bg-color rounded-circle show-group-color"></span>
+                                <div class="custom-control custom-radio mr-4 my-1">
+                                    <input type="radio" id="group_e" name="user_group" class="custom-control-input">
+                                    <label class="custom-control-label" for="group_e">Group E</label>
                                 </div>
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="group_start_date">Start date <span class="font-weight-light">( mm/dd/year )</span></label>
-                                <input type="date" class="form-control" id="group_start_date" name="group_start_date" value="2020-08-12" required>
+                                <label for="user_email">{{ __('User Email') }}</label>
+                                <input type="email" class="form-control" id="user_email" name="user_email" value="email@domain.com" required>
                             </div>
                         </div>
 
                         <div class="form-row">
                             <div class="form-group col-md-6">
-                                <label for="group_end_date">End date spa<span class="font-weight-light">( mm/dd/year )</span></label>
-                                <input type="date" class="form-control" id="group_end_date" name="group_end_date" value="2020-08-27" required>
+                                <label for="user_phone_no">{{ __('User Phone') }}</label>
+                                <input type="tel" class="form-control" id="user_phone_no" name="user_phone_no" value="+470156421" required>
                             </div>
                         </div>
 
                         <div class="form-row">
-                            <label for="" class="d-block w-100">Select group admins:</label>
+                            <label for="" class="d-block w-100">{{ __('Select primary contact:') }}</label>
 
                             <div class="form-group col-md-6">
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_1" checked>
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_1" checked>
                                     <label class="custom-control-label" for="group_admin_1">Adam Rusega</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_2" checked>
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_2">
                                     <label class="custom-control-label" for="group_admin_2">Levi Archman</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_3" checked>
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_3">
                                     <label class="custom-control-label" for="group_admin_3">Jhonny Ive</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_4">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_4">
                                     <label class="custom-control-label" for="group_admin_4">Douglas Costa</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_4" checked>
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_4">
                                     <label class="custom-control-label" for="group_admin_4">Eren Yeager</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_4">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_4">
                                     <label class="custom-control-label" for="group_admin_4">Bone ALi Mia</label>
                                 </div>
-                                <div class="custom-control custom-checkbox">
-                                    <input type="checkbox" class="custom-control-input" name="group_admins" id="group_admin_4">
+                                <div class="custom-control custom-radio">
+                                    <input type="radio" class="custom-control-input" name="group_admins" id="group_admin_4">
                                     <label class="custom-control-label" for="group_admin_4">Alan Walker</label>
                                 </div>
                             </div>
                         </div>
 
-                        <button type="submit" class="btn btn-primary">Save changes</button>
+                        <button type="submit" class="btn btn-primary">{{ __('Save Changes') }}</button>
                     </form>
                 </div>
             </div>
