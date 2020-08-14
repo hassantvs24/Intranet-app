fcdb302dd13bb1b411defb7ca64565a846a351b3
Ashikur Rahaman <ashik.developer@gmail.com>
2020-08-14 16:01:50 +0600

    Borad,Group,GroupAdmin Model and tables created

---
+++ database/migrations/2020_08_14_093027_create_group_admins_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGroupAdminsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('group_admins', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',100);
+            $table->string('email');
+            $table->string('phone');
+            $table->string('avatar')->default('default.jpg');
+            $table->text('bio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('group_admins');
+    }
+}
