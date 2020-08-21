3be9326a97415da012bca5fa083e8a232b56d1ec
walidmahade <walid_mahade@outlook.com>
2020-08-21 15:15:11 +0600

    boards edit page

--- resources/views/backend/boards/infocards.blade.php
+++ resources/views/backend/boards/infocards.blade.php
@@ -9,6 +9,7 @@
         <!-- START data cards -->
         <div class="data-cards">
             <div class="row">
+
                 <!-- START card -->
                 <div class="col col-md-4 my-4">
                     <div class="card">
@@ -33,7 +34,7 @@
                             </div>
 
                             <div class="custom-control custom-switch d-inline-block ml-auto">
-                                <input type="checkbox" class="custom-control-input" id="customSwitch1">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch1" checked>
                                 <label class="custom-control-label" for="customSwitch1">{{ __('Visibility') }}</label>
                             </div>
                         </div>
@@ -41,6 +42,7 @@
                     </div>
                 </div>
                 <!-- END card -->
+
                 <!-- START card -->
                 <div class="col col-md-4 my-4">
                     <div class="card">
@@ -60,9 +62,24 @@
                                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolore doloremque eaque enim eveniet.
                             </div>
                         </div>
+                        {{-- edit-options --}}
+                        <div class="card-footer mt-3 border-0 rounded-lg d-flex align-items-center">
+                            <div class="d-inline-block">
+                                <button class="btn btn-outline-primary btn-sm d-inline-block" id="btn-edit-group" data-toggle="modal" data-target="#staticBackdrop">
+                                    {{ __('Edit') }}
+                                </button>
+                            </div>
+
+                            <div class="custom-control custom-switch d-inline-block ml-auto">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch2" checked>
+                                <label class="custom-control-label" for="customSwitch2">{{ __('Visibility') }}</label>
+                            </div>
+                        </div>
+                        {{-- // edit-options --}}
                     </div>
                 </div>
                 <!-- END card -->
+
                 <!-- START card -->
                 <div class="col col-md-4 my-4">
                     <div class="card">
@@ -74,50 +91,65 @@
                                 <iframe width="100%" height="180" src="https://www.youtube.com/embed/L4qM1IEhtNQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                             </div>
                         </div>
+                        {{-- edit-options --}}
+                        <div class="card-footer mt-3 border-0 rounded-lg d-flex align-items-center">
+                            <div class="d-inline-block">
+                                <button class="btn btn-outline-primary btn-sm d-inline-block" id="btn-edit-group" data-toggle="modal" data-target="#staticBackdrop">
+                                    {{ __('Edit') }}
+                                </button>
+                            </div>
+
+                            <div class="custom-control custom-switch d-inline-block ml-auto">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch3" checked>
+                                <label class="custom-control-label" for="customSwitch3">{{ __('Visibility') }}</label>
+                            </div>
+                        </div>
+                        {{-- // edit-options --}}
                     </div>
                 </div>
                 <!-- END card -->
+
                 <!-- START card -->
-                <div class="col col-md-8 my-4">
+                <div class="col col-md-12 my-4">
                     <div class="card">
                         <div class="card-header bg-color">
-                            <h3 class="cart-title mb-0 txt-color">Calender</h3>
+                            <h3 class="cart-title mb-0 txt-color">{{ __('Calender') }}</h3>
                         </div>
                         <div class="card-body">
                             <div class="card-body__child card-body__child--text">
                                 <div id="calendar"></div>
                             </div>
                         </div>
+
+                        {{-- edit-options --}}
+                        <div class="card-footer mt-3 border-0 rounded-lg d-flex align-items-center">
+                            <div class="d-inline-block">
+                                <button class="btn btn-outline-primary btn-sm d-inline-block" id="edit-calender-btn">
+                                    {{ __('Edit') }}
+                                </button>
+                                <button class="btn btn-secondary btn-sm d-inline-block" id="save-calender-btn" disabled>
+                                    {{ __('Save') }}
+                                </button>
+                            </div>
+
+                            <div class="custom-control custom-switch d-inline-block ml-auto">
+                                <input type="checkbox" class="custom-control-input" id="customSwitch4" checked>
+                                <label class="custom-control-label" for="customSwitch4">{{ __('Visibility') }}</label>
+                            </div>
+                        </div>
+                        {{-- // edit-options --}}
                     </div>
                 </div>
                 <!-- END card -->
-                <!-- START card -->
-                <div class="col col-md-4 my-4">
-                    <div class="card">
-                        <div class="card-header bg-color">
-                            <h3 class="cart-title mb-0 txt-color">Primary contact</h3>
-                        </div>
-                        <div class="card-body">
-                            <div class="card-body__child card-body__child--img">
-                                <img class="rounded-circle d-block m-auto" height="100" width="100" src="/images/demo-card-image.png" alt="">
-                            </div>
-                            <div class="card-body__child card-body__child--text text-center">
-                                <div class="name font-weight-bold">Contact Name</div>
-                                <div class="name font-weight-bold">email@domain.com</div>
-                                <div class="name font-weight-bold">+8801994961651</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- END card -->
+
             </div>
         </div>
         <!-- END data cards -->
     </div>
 
     {{-- ============= template for editing cards - modal ============= --}}
-    <div class="modal fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+    <div class="modal right fade" id="staticBackdrop" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog">
             <div class="modal-content border-0 px-4">
 
             <div class="modal-header border-0">
@@ -148,10 +180,32 @@
         </div>
     </div>
 
-    {{-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> --}}
+    {{-- ====== template - modal - primary contact ====== --}}
+    <div id="primary-contact-edit-template" class="template">
+        <div class="card">
+            <div class="card-body">
+                <form action="" method="post">
+                    <div class="form-row">
+                        <div class="form-group">
+                            <label for=""></label>
+                            <select class="custom-select">
+                                <option selected>Open this select menu</option>
+                                <option value="1">One</option>
+                                <option value="2">Two</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    {{-- // ====== template - modal - primary contact ====== --}}
+
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote-lite.min.js"></script>
+
     <script>
         (function($) {
             $('#summernote').summernote({
--- resources/views/layouts/app-admin.blade.php
+++ resources/views/layouts/app-admin.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }} - @yield('title')</title>
     <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/admin.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
