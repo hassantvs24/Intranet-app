122c638bc6c5b5e1d6247d1b4b0e6dd10ab129e7
walidmahade <wali_dmahade@outlook.com>
2020-08-26 17:00:53 +0600

    added animation to login signup

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -8,9 +8,25 @@
     <div class="register-page-wrap">
         <div class="container-fluid">
             <div class="row justify-content-center align-items-stretch align-content-stretch">
+                
                 <div class="col-md-5 bg-dark left-col d-flex flex-wrap justify-content-center align-content-center align-items-center">
-                    <h1 class="text-center text-light w-100 font-weight-bolder">{{ __('Register.') }}</h1>
-                    <p class="w-100 text-center text-light">{{ __('Register for a new account using this form.') }}</p>
+                    {{-- animations --}}
+                    <div class="animation-overlay">
+                        <div class="xmark xmark-1"> <div class="h-line"></div> <div class="v-line"></div> </div>
+                        <div class="xmark xmark-2"> <div class="h-line"></div> <div class="v-line"></div> </div>
+                        <div class="xmark xmark-3"> <div class="h-line"></div> <div class="v-line"></div> </div>
+                        <div class="xmark xmark-4"> <div class="h-line"></div> <div class="v-line"></div> </div>
+                        {{-- <div class="xmark xmark-2"> <div class="h-line"></div> <div class="v-line"></div> </div> --}}
+                        <div class="circle circle-1"></div>
+                        <div class="circle circle-2"></div>
+                        <div class="circle circle-3"></div>
+                        <div class="line line-1"></div>
+                    </div> 
+                    {{-- // animations --}}
+                    <div class="content">
+                        <h1 class="text-center text-light w-100 font-weight-bolder">{{ __('Register.') }}</h1>
+                        <p class="w-100 text-center text-light">{{ __('Register for a new account using this form.') }}</p>
+                    </div>
                 </div>
 
                 <div class="col-md-7 right-col d-flex flex-wrap justify-content-center align-content-center align-items-center">
