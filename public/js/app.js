ace933dd8cc36984932931ef84aaf463e8bc8d75
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-10-21 17:42:05 +0600

    Update

--- public/js/app.js
+++ public/js/app.js
@@ -51492,9 +51492,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\walid\work\laragon\www\Intranet-app\resources\js\app.js */"./resources/js/app.js");
-__webpack_require__(/*! C:\Users\walid\work\laragon\www\Intranet-app\resources\sass\app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! C:\Users\walid\work\laragon\www\Intranet-app\resources\sass\admin.scss */"./resources/sass/admin.scss");
+__webpack_require__(/*! C:\wamp64\www\Intranet-app\resources\js\app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\wamp64\www\Intranet-app\resources\sass\app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! C:\wamp64\www\Intranet-app\resources\sass\admin.scss */"./resources/sass/admin.scss");
 
 
 /***/ })
