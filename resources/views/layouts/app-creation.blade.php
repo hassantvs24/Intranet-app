417ffe17b3cf5e1f49b83d72b17ec86f0410f931
Mahade <walid_mahade@outlook.com>
2020-10-23 11:00:28 +0600

    added insta and fb icons to user dashboard

--- resources/views/layouts/app-creation.blade.php
+++ resources/views/layouts/app-creation.blade.php
@@ -96,6 +96,8 @@
 </div>
 
 <div id="credits" style="display: none">
+    <a href="https://iconscout.com/icons/instagram" target="_blank">Instagram Icon</a> by <a href="https://iconscout.com/contributors/maninderkaur" target="_blank">Maninder Kaur</a>
+    <a href="https://iconscout.com/icons/facebook" target="_blank">Facebook Icon</a> by <a href="https://iconscout.com/contributors/ipost">I Post</a> on <a href="https://iconscout.com">Iconscout</a>
     <a href="https://iconscout.com/icons/dashboard" target="_blank">Dashboard Icon</a> by <a href="https://iconscout.com/contributors/google-inc">Google Inc.</a> on <a href="https://iconscout.com">Iconscout</a>
     <a href="https://iconscout.com/icons/people" target="_blank">People Icon</a> by <a href="https://iconscout.com/contributors/fluent">Microsoft</a> on <a href="https://iconscout.com">Iconscout</a>
     <a href="https://iconscout.com/icons/user" target="_blank">User Icon</a> by <a href="https://iconscout.com/contributors/kawalanicon">Kawalan Icon</a> on <a href="https://iconscout.com">Iconscout</a>
